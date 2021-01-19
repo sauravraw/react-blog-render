15fdef8add71a093e12d8d6dd2f29adffc90ab74
saurav <sourav.upadhyay@raweng.com>
2021-01-19 20:15:40 +0530

    size fixed

--- src/components/404Error/404Error.jsx
+++ src/components/404Error/404Error.jsx
@@ -5,9 +5,16 @@ import image from "../../images/404.gif";
 export default class ErorrPage extends Component {
 	render() {
 		return (
-			<div style={{ marginTop: "100px", marginLeft: "100px" }}>
+			<div
+				style={{
+					marginTop: "45px",
+					marginLeft: "300px",
+					marginBottom: "-45px",
+				}}
+			>
 				<img
 					src={image}
+					style={{ width: "73%" }}
 					className={style["container-image"]}
 					alt="404 Page Not Found"
 				/>
